9950f8fc160313d1eea6aa0def952aea77a6da13
yokuny <felipe.vni@hotmail.com>
2023-01-19 01:07:29 -0300

    axios function test

--- src/script/main.js
+++ src/script/main.js
@@ -1,18 +1,49 @@
-// Para entrar. enviar ao servidor o nome do usuário
-// envie uma requisição `POST` para a URL:
-// https://mock-api.driven.com.br/api/v6/uol[/participants](https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants) 
-    
-// Enviando um objeto no formato:
-// {
-//     name: "João"
-// }
-// status 400 se já houver um usuário online com esse nome.
-// Se for o caso, pedir um novo nome até status 200.
 
-// Saber que o usuário continua online. enviar uma requisição `POST` para a URL:
-// https://mock-api.driven.com.br/api/v6/uol/status
-// Enviando objeto  nome do usuário que foi pedido ao entrar
-//     {
-//       name: "João"
-//     }
-// a cada cinco segundos.
+const user = (name) => {
+    return {name};
+}
+let userName = user(prompt("digite seu nome:"));
+// new user
+function niceServerReturn(succesReturn){
+    console.log(succesReturn.status);
+}
+function badServerReturn(badReturn){
+    console.log(badReturn);
+}
+const newUser = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', userName);
+newUser.then(niceServerReturn);
+newUser.catch(badServerReturn);
+// enviando msg
+const sendConfirm = axios.get(
+    "https://mock-api.driven.com.br/api/v6/uol/messages",
+    {
+        from: "nome do usuário",
+        to: "nome do destinatário (Todos se não for um específico)",
+        text: "mensagem digitada",
+        type: "message" // ou "private_message" para o bônus
+    }
+);
+// verificando online
+function userOnline(response){
+    console.log("response COM usuarios online")
+    console.log(response);
+    setTimeout(hasUsers, 5000);
+}
+function noOnline(response){
+    console.log("response SEM usuarios online")
+    console.log(response);
+    setTimeout(hasUsers, 5000);
+}
+function hasUsers(){
+    const activeUser = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', userName);
+    activeUser.then(userOnline);
+    activeUser.catch(noOnline);
+}
+hasUsers();
+// puxar msg
+const serverMsgs = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+// Nos objetos, campo type identifica o tipo da mensagem. Existem os valores:
+// `status`: mensagem de estado, 'quem entrou ou saiu da sala;
+// `message`: mensagem pública;
+// `private_message`: mensagem particular.
+
