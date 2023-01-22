4f0cad0a87d1b39b4a6e8f3b6f4b86c5eead24af
yokuny <felipe.vni@hotmail.com>
2023-01-22 14:22:47 -0300

    Refatored

--- src/script/main.js
+++ src/script/main.js
@@ -1,134 +1,118 @@
-const inputMsg = document.getElementById("newMsg");
 const newUser = document.getElementById("newUserName");
+const inputMsg = document.getElementById("newMsg");
+const showMenu = () => (document.getElementById("asideMenu").style.display = "flex");
+const menuExit = () => (document.getElementById("asideMenu").style.display = "none");
 
-const user = (name) => ({ name });
-let userName;
-const msg = (from, text, to = "Todos", type = "message") => ({
-  from,
-  to,
-  text,
-  type,
-});
-// {
-//     from: "nome do usuário",
-//     to: "nome do destinatário (Todos se não for um específico)",
-//     text: "mensagem digitada",
-//     type: "message" // ou "private_message" para o bônus
-// }
+const core = (() => {
+  const user = (name) => ({ name });
+  const msg = (from, text, to = "Todos", type = "message") => ({ from, to, text, type });
 
-// newUser
-const newServerUser = (user) => {
-  const newUserToServer = axios.post(
-    "https://mock-api.driven.com.br/api/v6/uol/participants",
-    user
-  );
-  newUserToServer.then((succesReturn) => {
-    document.getElementById("registerScreen").style.display = "none";
-    console.log(`Registro de novo usuario. Codigo: ${succesReturn.status}`);
-  });
-  newUserToServer.catch((badReturn) =>
-    alert(
-      `Tivemos problema criando novo usuario. Codigo de erro: ${badReturn.response.status}`
-    )
-  );
-};
+  function getUserName() {
+    const inputUserName = document.getElementById("userName").value;
+    return user(inputUserName);
+  }
+
+  function consoleMsg(text, status) {
+    console.log(text + " " + status);
+  }
+
+  const newServerUser = () => {
+    const newUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", getUserName());
+
+    newUser.then((succesReturn) => {
+      document.getElementById("registerScreen").style.display = "none";
+      consoleMsg("Registro de novo usuario. Codigo:", succesReturn.status);
+    });
+    newUser.catch((badReturn) => {
+      const input = document.getElementById("userName");
+      input.style.border = "solid 2px red";
+      consoleMsg("Tivemos problema criando novo usuario. Codigo de erro:", badReturn.response.status);
+    });
+  };
+
+  const newUserMsg = () => {
+    const msgInput = document.getElementById("userMsgInput");
+    const theMsg = msg(getUserName().name, msgInput.value);
+    console.log(theMsg);
+    const newMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", theMsg);
+
+    newMsg.then((status) => {
+      msgInput.value = "";
+      consoleMsg("Mensagem enviada. Codigo:", status.status);
+    });
+    newMsg.catch((badReturn) => {
+      const input = document.getElementById("userMsgInput");
+      input.style.border = "solid 2px red";
+      consoleMsg("Erro no envio da mensagem. Codigo:", badReturn.response.status);
+    });
+  };
+
+  function render(msg) {
+    const ulField = document.getElementById("msgBody");
+    ulField.innerHTML = "";
+    msg.forEach((element) => {
+      if (element.to != "Todos") {
+        ulField.innerHTML += `
+          <li class="privateMessage">
+              <span class="info">(${element.time})</span>
+              <span class="negrito">${element.from}</span>
+              <span>reservadamente para</span>
+              <span class="negrito">${element.to}:</span>
+              <span>${element.text}</span>
+          </li>`;
+      } else if (element.type == "status") {
+        ulField.innerHTML += `
+          <li class="userStatus">
+              <span class="info">(${element.time})</span>
+              <span class="negrito">${element.from}</span>
+              <span>${element.text}</span>
+          </li>`;
+      } else if (element.type == "message") {
+        ulField.innerHTML += `
+          <li class="userMessage">
+              <span class="info">(${element.time})</span>
+              <span class="negrito">${element.from}</span>
+              <span>${element.text}</span>
+          </li>`;
+      }
+    });
+  }
+
+  const msgsRequire = () => {
+    const serverMsgs = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    serverMsgs.then((response) => {
+      render(response.data);
+      consoleMsg("Novas msg recebidas. Codigo:", response.status);
+    });
+    serverMsgs.catch((badReturn) => consoleMsg("Sem novas msg. Codigo:", badReturn.response.status));
+  };
+
+  const userConnection = () => {
+    const activeUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", getUserName());
+
+    activeUser.then((status) => {
+      consoleMsg("Usuario online. Codigo:", status.status);
+      msgsRequire();
+      setTimeout(userConnection, 5000);
+    });
+    activeUser.catch((badReturn) => {
+      msgsRequire("Usuario offline. Codigo:", badReturn.response.status);
+      setTimeout(userConnection, 5000);
+    });
+  };
+
+  return { newServerUser, newUserMsg, userConnection, msgsRequire };
+})();
+
 newUser.addEventListener("submit", (event) => {
   event.preventDefault();
-  const userNameInput = document.getElementById("userName").value;
-  userName = user(userNameInput);
-  // new user in server
-  newServerUser(userName);
+  core.newServerUser();
+  core.userConnection();
 });
 
-// enviando msg
 inputMsg.addEventListener("submit", (event) => {
   event.preventDefault();
-  const msgInput = document.getElementById("userMsgInput");
-  const newMsg = msg(userName.name, msgInput.value);
-  const sendConfirmation = axios.post(
-    "https://mock-api.driven.com.br/api/v6/uol/messages",
-    newMsg
-  );
-  sendConfirmation.then((status) => {
-    msgInput.value = "";
-    console.log(`> Mensagem enviada. Codigo: ${status.status}, ${newMsg.text}`);
-  });
-  sendConfirmation.catch((badReturn) => {
-    // personalizar o input
-    console.log(
-      `> Erro no envio da mensagem. Codigo: ${badReturn.response.status}`
-    );
-  });
+  core.newUserMsg();
+  core.userConnection();
 });
-
-function render(msg) {
-  console.log(msg);
-  const ulField = document.getElementById("msgBody");
-  ulField.innerHTML = "";
-  msg.forEach((element) => {
-    if (element.to != "Todos") {
-      ulField.innerHTML += `
-        <li class="privateMessage">
-            <span class="info">(${element.time})</span>
-            <span class="negrito">${element.from}</span>
-            <span>reservadamente para</span>
-            <span class="negrito">${element.to}:</span>
-            <span>${element.text}</span>
-        </li>`;
-    } else if (element.type == "status") {
-      ulField.innerHTML += `
-        <li class="userStatus">
-            <span class="info">(${element.time})</span>
-            <span class="negrito">${element.from}</span>
-            <span>${element.text}</span>
-        </li>`;
-    } else if (element.type == "message") {
-      ulField.innerHTML += `
-        <li class="userMessage">
-            <span class="info">(${element.time})</span>
-            <span class="negrito">${element.from}</span>
-            <span>${element.text}</span>
-        </li>`;
-    }
-  });
-}
-
-// puxar msg
-const newMsgCheck = () => {
-  const serverMsgs = axios.get(
-    "https://mock-api.driven.com.br/api/v6/uol/messages"
-  );
-  serverMsgs.then((response) => {
-    render(response.data);
-    console.log(`Novas msg recebidas. Codigo: ${response.status}`);
-  });
-  serverMsgs.catch((badReturn) =>
-    console.log(`Sem novas msg. Codigo: ${badReturn.response.status}`)
-  );
-};
-
-// verificando online
-const userOnline = () => {
-  const activeUser = axios.post(
-    "https://mock-api.driven.com.br/api/v6/uol/status",
-    userName
-  );
-  activeUser.then((status) => {
-    console.log(`Ainda há usuarios online. Codigo: ${status.status}`);
-    newMsgCheck();
-    setTimeout(userOnline, 5000);
-  });
-  activeUser.catch((badReturn) => {
-    console.log(`Sem usuarios online. Codigo: ${badReturn.response.status}`);
-    setTimeout(userOnline, 5000);
-  });
-};
-userOnline();
-
-// menus
-function showMenu() {
-  document.getElementById("asideMenu").style.display = "flex";
-}
-function menuExit() {
-  document.getElementById("asideMenu").style.display = "none";
-}
