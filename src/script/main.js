cff3abd2946a8e86303384cfcd41f8df4ccc5c11
yokuny <felipe.vni@hotmail.com>
2023-01-23 17:52:00 -0300

    last changes

--- src/script/main.js
+++ src/script/main.js
@@ -3,26 +3,57 @@ const core = (() => {
   let connectionCheckTimmer = null;
   let usersOnlineCheckTimmer = null;
   const user = (name) => ({ name });
-  const msg = (from, text, to = "Todos", type = "message") => ({ from, to, text, type });
+  const msg = (from, text, to, type) => {
+    if (to == null) to = "Todos";
+    return { from, to, text, type };
+  };
   const onlineNames = (name, to) => ({ name, to });
+
   const userMenu = () => {
     let msgTo = null;
-    let msgVisibilite = 1;
+    let msgType = "message";
     let usersOnline = [];
     function userOnline(playerList) {
       usersOnline.length = 0;
-      console.log(usersOnline);
       playerList.forEach((item) => {
         if (item.name == msgTo) {
           usersOnline.push(onlineNames(item.name, 1));
         } else {
+          // console.log(msgTo);
+          // console.log(item.name);
           usersOnline.push(onlineNames(item.name, 0));
         }
       });
     }
-    return { msgTo, msgVisibilite, usersOnline, userOnline };
+    return { msgTo, msgType, usersOnline, userOnline };
   };
   let users = userMenu();
+
+  const toAllOrPrivate = (value) => {
+    users.msgType = value;
+    usersOnline();
+  };
+  const someOneOrAll = (value) => {
+    users.msgTo = value;
+    usersOnline();
+  };
+
+  const newUserMsg = () => {
+    const msgInput = document.getElementById("userMsgInput");
+    const theMsg = msg(getUserName().name, msgInput.value, users.msgTo, users.msgType);
+    console.log(theMsg);
+    const newMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", theMsg);
+    newMsg.then((status) => {
+      msgInput.value = "";
+      requestMessages();
+      consoleMsg("Mensagem enviada. Codigo:", status.status);
+    });
+    newMsg.catch((badReturn) => {
+      const input = document.getElementById("userMsgInput");
+      input.style.border = "solid 2px red";
+      consoleMsg("Erro no envio da mensagem. Codigo:", badReturn.response.status);
+    });
+  };
 
   function resetRequestMessages() {
     if (msgCheckTimmer) {
@@ -49,27 +80,27 @@ const core = (() => {
   function msgVisibilite(value) {
     const msgVisibilite = document.getElementById("msgTo");
     msgVisibilite.innerHTML = "";
-    if (value == 1) {
+    if (value == "message") {
       msgVisibilite.innerHTML = `
-      <span data-test="public">
+      <span id="message" data-test="public" onclick="msgType(this)">
         <ion-icon name="lock-open"></ion-icon>
         <span>Público</span>
         <ion-icon class="menuIconDisplay" name="checkmark-sharp" data-test="check"></ion-icon>
       </span>
-      <span data-test="public">
+      <span id="private_message" data-test="public" onclick="msgType(this)">
         <ion-icon name="lock-closed"></ion-icon>
         <span>Reservadamente</span>
         <ion-icon name="checkmark-sharp" data-test="check"></ion-icon>
       </span>
       `;
-    } else {
+    } else if (value == "private_message") {
       msgVisibilite.innerHTML = `
-      <span data-test="public">
+      <span id="message" data-test="public" onclick="msgType(this)">
         <ion-icon name="lock-open"></ion-icon>
         <span>Público</span>
         <ion-icon name="checkmark-sharp" data-test="check"></ion-icon>
       </span>
-      <span data-test="public">
+      <span id="private_message" data-test="public" onclick="msgType(this)">
         <ion-icon name="lock-closed"></ion-icon>
         <span>Reservadamente</span>
         <ion-icon class="menuIconDisplay" name="checkmark-sharp" data-test="check"></ion-icon>
@@ -80,35 +111,37 @@ const core = (() => {
   function usersRender(user) {
     const usersField = document.getElementById("usersOnline");
     usersField.innerHTML = "";
+    console.log(user);
     if (!users.msgTo) {
       usersField.innerHTML = `
-      <span id="forAll" data-test="all">
+      <span id="forAll" data-test="all" onclick="msgTo(this)">
         <ion-icon name="people-sharp"></ion-icon>
         <span>Todos</span>
         <ion-icon class="menuIconDisplay" name="checkmark-sharp" data-test="check"></ion-icon>
       </span>`;
     } else {
       usersField.innerHTML = `
-      <span id="forAll" data-test="all">
+      <span id="forAll" data-test="all" onclick="msgTo(this)">
         <ion-icon name="people-sharp"></ion-icon>
         <span>Todos</span>
         <ion-icon name="checkmark-sharp" data-test="check"></ion-icon>
       </span>`;
     }
-    user.forEach((user) => {
-      if (user.to == 0) {
+    user.usersOnline.forEach((fulano) => {
+      console.log(fulano);
+      if (fulano.name == user.msgTo) {
         usersField.innerHTML += `
-          <span id="${user.name}" data-test="participant">
+          <span id="${fulano.name}" data-test="participant" onclick="msgTo(this)">
             <ion-icon name="person-circle"></ion-icon>
-            <span>${user.name}</span>
-            <ion-icon name="checkmark-sharp" data-test="check"></ion-icon>
+            <span>${fulano.name}</span>
+            <ion-icon class="menuIconDisplay" name="checkmark-sharp" data-test="check"></ion-icon>
           </span>`;
       } else {
         usersField.innerHTML += `
-          <span id="${user.name}" data-test="participant">
+          <span id="${fulano.name}" data-test="participant" onclick="msgTo(this)">
             <ion-icon name="person-circle"></ion-icon>
-            <span>${user.name}</span>
-            <ion-icon class="menuIconDisplay" name="checkmark-sharp" data-test="check"></ion-icon>
+            <span>${fulano.name}</span>
+            <ion-icon name="checkmark-sharp" data-test="check"></ion-icon>
           </span>`;
       }
     });
@@ -118,8 +151,8 @@ const core = (() => {
     const allUsers = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
     allUsers.then((status) => {
       users.userOnline(status.data);
-      usersRender(users.usersOnline);
-      msgVisibilite(users.msgVisibilite);
+      usersRender(users);
+      msgVisibilite(users.msgType);
       resetRequestUsersOnline();
       consoleMsg("Total de usuarios. Codigo:", status.status);
     });
@@ -138,7 +171,6 @@ const core = (() => {
 
   const login = () => {
     const newUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", getUserName());
-
     newUser.then((succesReturn) => {
       document.getElementById("registerScreen").style.display = "none";
       userConnection();
@@ -154,23 +186,6 @@ const core = (() => {
         input.placeholder = "Nome já registrado.";
       }
       consoleMsg("Tivemos problema criando novo usuario. Codigo de erro:", badReturn.response.status);
-    });
-  };
-
-  const newUserMsg = () => {
-    const msgInput = document.getElementById("userMsgInput");
-    const theMsg = msg(getUserName().name, msgInput.value);
-    msgInput.value = "";
-    const newMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", theMsg);
-
-    newMsg.then((status) => {
-      requestMessages();
-      consoleMsg("Mensagem enviada. Codigo:", status.status);
-    });
-    newMsg.catch((badReturn) => {
-      const input = document.getElementById("userMsgInput");
-      input.style.border = "solid 2px red";
-      consoleMsg("Erro no envio da mensagem. Codigo:", badReturn.response.status);
     });
   };
 
@@ -210,7 +225,6 @@ const core = (() => {
 
   function requestMessages() {
     const serverMsgs = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-
     serverMsgs.then((response) => {
       msgRender(response.data);
       resetRequestMessages();
@@ -218,10 +232,8 @@ const core = (() => {
     });
     serverMsgs.catch((badReturn) => consoleMsg("Sem novas msg. Codigo:", badReturn.response.status));
   }
-
   const userConnection = () => {
     const activeUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", getUserName());
-
     activeUser.then((status) => {
       consoleMsg("Usuario online. Codigo:", status.status);
       resetRequestConnection();
@@ -231,10 +243,14 @@ const core = (() => {
       window.location.reload();
     });
   };
-
-  return { login, newUserMsg, userConnection, usersOnline };
+  const whisperTo = () => {
+    if (users.msgTo != null) {
+      const whisper = document.getElementById("whisperTo");
+      whisper.value = `Enviando para ${msgTo}`;
+    }
+  };
+  return { login, newUserMsg, userConnection, usersOnline, toAllOrPrivate, someOneOrAll, whisperTo };
 })();
-
 const newUser = document.getElementById("newUserName");
 const formInput = document.getElementById("newMsg");
 const inputTextMsg = document.getElementById("userMsgInput");
@@ -254,9 +270,18 @@ inputTextMsg.addEventListener("keyup", (event) => {
     core.userConnection();
   }
 });
-
 const showMenu = () => {
   document.getElementById("asideMenu").style.display = "flex";
   core.usersOnline();
 };
-const menuExit = () => (document.getElementById("asideMenu").style.display = "none");
+const menuExit = () => {
+  document.getElementById("asideMenu").style.display = "none";
+  core.whisperTo();
+};
+
+function msgTo(element) {
+  core.someOneOrAll(element.id);
+}
+function msgType(element) {
+  core.toAllOrPrivate(element.id);
+}
